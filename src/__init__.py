c0e81364e43e95fe83dd8f43110c1d4dac23da11
Q220036 <j.fransolet@student.helmo.be>
2025-01-03 15:08:19 +0100

    Add `__init__.py` files to `src` and `tests` directories
    These files enable the respective directories to function as Python modules. Imported modules include `domains`, `infrastructure`, and `utils`, ensuring proper access throughout the codebase.

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+﻿# src\__init__.py
+
+from domains import *
+from infrastructure import *
+from utils import *
+from interface import *
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+﻿# tests\__init__.py
+
+from domains import *
+from infrastructure import *
+from utils import *
