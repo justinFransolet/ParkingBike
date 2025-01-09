2e2f547f0c226b2e64860724c5d078ac5a0fb27c
Q220036 <j.fransolet@student.helmo.be>
2025-01-09 15:23:11 +0100

    Add JSONManager enhancements and comprehensive test coverage
    Implemented new methods in JSONManager to handle appearance, color theme, and language properties, with error handling for missing keys. Added unit tests to validate getters, setters, and scenarios with missing or existing values. Included necessary test files for valid, missing, and rewriting cases.

--- src/infrastructure/JSONManager.py
+++ src/infrastructure/JSONManager.py
@@ -22,10 +22,49 @@ class JSONManager:
 
         :return: the database path.
         """
-        data = self.__data["database"]
-        if data is None:
+        try:
+            return self.__data["db_path"]
+        except KeyError:
             raise ValueError("Database path is not found in the JSON file.")
-        return data
+
+    def get_appearance(self) -> str:
+        """
+        Get the name of the appearance.
+
+        :raise ValueError: if the name of the appearance is not found in the JSON file.
+
+        :return: the name of the appearance.
+        """
+        try:
+            return self.__data["style"]["appearance"]
+        except KeyError:
+            raise ValueError("The name of the appearance is not found in the JSON file.")
+
+    def get_color_theme(self) -> str:
+        """
+        Get the color theme.
+
+        :raise ValueError: if the color theme is not found in the JSON file.
+
+        :return: the color theme.
+        """
+        try:
+            return self.__data["style"]["color_theme"]
+        except KeyError:
+            raise ValueError("Color theme is not found in the JSON file.")
+
+    def get_language(self) -> str:
+        """
+        Get the language.
+
+        :raise ValueError: if the language is not found in the JSON file.
+
+        :return: the language.
+        """
+        try:
+            return self.__data["lang"]
+        except KeyError:
+            raise ValueError("Language is not found in the JSON file.")
 
     def set_database_path(self, path: str) -> None:
         """
@@ -33,5 +72,40 @@ class JSONManager:
 
         :param path: the new database path.
         """
-        self.__data["database"] = path
+        self.__data["db_path"] = path
+        write_file(self.__path, self.__data)
+
+    def set_appearance(self, appearance: str) -> None:
+        """
+        Set the appearance.
+
+        :param appearance: the new appearance.
+        """
+        try:
+            self.__data["style"]["appearance"] = appearance
+        except KeyError:
+            self.__data["style"] = {}
+            self.__data["style"]["appearance"] = appearance
+        write_file(self.__path, self.__data)
+
+    def set_color_theme(self, color: str) -> None:
+        """
+        Set the color theme.
+
+        :param color: the new color theme.
+        """
+        try:
+            self.__data["style"]["color_theme"] = color
+        except KeyError:
+            self.__data["style"] = {}
+            self.__data["style"]["color_theme"] = color
+        write_file(self.__path, self.__data)
+
+    def set_language(self, language: str) -> None:
+        """
+        Set the language.
+
+        :param language: the new langue.
+        """
+        self.__data["lang"] = language
         write_file(self.__path, self.__data)
---
+++ tests/infrastructure/JSONManagerTests.py
@@ -0,0 +1,73 @@
+﻿import unittest
+from src.infrastructure.JSONManager import *
+
+class JSONManagerTests(unittest.TestCase):
+    def setUp(self):
+        self.manager_valid = JSONManager("test_file/test_valid.json")
+        self.manager_missing = JSONManager("test_file/test_missing.json")
+        self.manager_rewriting = JSONManager("test_file/test_rewriting.json")
+    def test_get_params_when_there_in_the_file(self):
+        self.assertEqual(self.manager_valid.get_database_path(), "database.db")
+        self.assertEqual(self.manager_valid.get_appearance(), "dark")
+        self.assertEqual(self.manager_valid.get_color_theme(), "dark-blue")
+        self.assertEqual(self.manager_valid.get_language(), "en")
+    def test_get_params_when_there_not_in_the_fill(self):
+        with self.assertRaises(ValueError):
+            self.manager_missing.get_database_path()
+        with self.assertRaises(ValueError):
+            self.manager_missing.get_appearance()
+        with self.assertRaises(ValueError):
+            self.manager_missing.get_color_theme()
+        with self.assertRaises(ValueError):
+            self.manager_missing.get_language()
+    def test_changes_params_when_there_already_not_exist_in_the_file(self):
+        # Database path
+        path = "database.db"
+        self.manager_missing.set_database_path(path)
+        self.assertEqual(path,self.manager_missing.get_database_path())
+
+        # Appearance
+        appearance = "white"
+        self.manager_missing.set_appearance(appearance)
+        self.assertEqual(appearance, self.manager_missing.get_appearance())
+
+        # Color theme
+        color = "green"
+        self.manager_missing.set_color_theme(color)
+        self.assertEqual(color, self.manager_missing.get_color_theme())
+
+        # Language
+        lang = "fr"
+        self.manager_missing.set_language(lang)
+        self.assertEqual(lang, self.manager_missing.get_language())
+
+        with open("test_file/test_missing.json", 'w', encoding='utf-8-sig') as file:
+            json.dump({}, file, indent=4, ensure_ascii=False)
+
+    def test_changes_params_when_there_already_exist_in_the_file(self):
+        # Database path
+        old_path = self.manager_rewriting.get_database_path()
+        self.manager_rewriting.set_database_path("database2.db")
+        self.assertNotEqual(old_path,self.manager_rewriting.get_database_path())
+        self.manager_rewriting.set_database_path(old_path)
+
+        # Appearance
+        old_appearance = self.manager_rewriting.get_appearance()
+        self.manager_rewriting.set_appearance("white")
+        self.assertNotEqual(old_appearance, self.manager_rewriting.get_appearance())
+        self.manager_rewriting.set_appearance(old_appearance)
+
+        # Color theme
+        old_color = self.manager_rewriting.get_color_theme()
+        self.manager_rewriting.set_color_theme("green")
+        self.assertNotEqual(old_color, self.manager_rewriting.get_color_theme())
+        self.manager_rewriting.set_color_theme(old_color)
+
+        # Language
+        old_lang = self.manager_rewriting.get_language()
+        self.manager_rewriting.set_language("fr")
+        self.assertNotEqual(old_lang, self.manager_rewriting.get_language())
+        self.manager_rewriting.set_language(old_lang)
+
+if __name__ == '__main__':
+    unittest.main()
